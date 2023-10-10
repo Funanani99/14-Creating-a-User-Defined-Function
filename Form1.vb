6b15e6a843031a61acb849c4b38709ad057c6541
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:07:55 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Public Function BMI(Height As Single, weight As Single) As Double
+        BMI = weight / Height ^ 2
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim h, w As Single
+        h = Val(TextBox1.Text)
+        w = Val(TextBox2.Text)
+        ' TextBox3.Text = Format(BMI(h, w), "0.00")
+        TextBox3.Text = Math.Round(BMI(h, w), 2)
+    End Sub
+End Class
